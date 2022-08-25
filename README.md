7de8b9e8e006b8f22c82a850f1f81bc83ded25d7
Дмитрий Пируев <peryevd1@gmail.com>
2022-08-25 18:00:31 +0300

    DEV: Install from file

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-### Install
+### Install:
 
-  - docker-compose up
-  - run ```./set_configuration.sh```
-  - go to container "app-server",
-  - open "config/config.php" and add your ip to trusted_domains (172.22.115.221)
-  - run ```apt-get install smbclient```
-  - restart the container
-  - go to nextcloud, apps, and install "ONLYOFFICE" and "Full text search", "Full text search - Elasticsearch Platform", "Full text search - Files"
-  - go to settings - ONLYOFFICE, enter "http://ip/" in address
-  - go to settings - Full text search, enter platform Elasticsearch, enter addres servlet - http://elasticsearch_nc:9200
-  - enter index: my_index  
-  - run ```sudo docker exec -u www-data app-server php /var/www/html/occ fulltextsearch:index```
- 
-### Convert db type
-```sudo docker exec -u www-data app-server php occ db:convert-type --all-apps --password="nextcloud" mysql nextcloud 192.168.96.6 nextcloud```
+Run 
+``` bash
+./set_configuration.sh
+```
+
+Work
+ - go to http://localhost:88
--- set_configuration.sh
+++ set_configuration.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
 set -x
+
+docker-compose up -d
+sleep 20
+# docker exec -u www-data app-server /bin/bash -c "export OC_PASS=AnswerPRO! && php occ user:add --password-from-env --display-name='Administrator' --group='admins' admin"
+docker exec -u www-data app-server /bin/bash -c 'php occ  maintenance:install --admin-user "admin" --admin-pass "AnswerPRO!"'
 
 docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
 
@@ -11,8 +15,22 @@ fi
 
 rm trusted_domain.tmp
 
+# docker exec -u root app-server /bin/bash -c "apt-get update && apt-get install smbclient"
+# docker restart app-server
+
 docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
+docker exec -u www-data app-server php occ --no-warnings app:install fulltextsearch
+docker exec -u www-data app-server php occ --no-warnings app:install fulltextsearch_elasticsearch
+docker exec -u www-data app-server php occ --no-warnings app:install files_fulltextsearch
+docker exec -u www-data app-server php occ --no-warnings app:enable files_external
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+
+docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch search_platform --value="OCA\\FullTextSearch_Elasticsearch\\Platform\\ElasticSearchPlatform"
+docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch app_navigation --value="1"
+docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch_elasticsearch elastic_host --value="http://elasticsearch_nc:9200/"
+docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch_elasticsearch elastic_index --value="my_index"
+
+docker exec -u www-data app-server php /var/www/html/occ fulltextsearch:index
