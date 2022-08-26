2ebbfbc21816b3867102cbc1ceb11fa6f8b2de18
Дмитрий Пируев <peryevd1@gmail.com>
2022-08-26 16:16:25 +0300

    WIP

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ docker exec -u www-data app-server php occ --no-warnings config:system:get trust
 
 if ! grep -q "nginx-server" trusted_domain.tmp; then
     TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
-    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
+    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nextcloud_nginx.1.41d2jknvpgnhdmr3kkdlp8cbt"
 fi
 
 rm trusted_domain.tmp
@@ -29,12 +29,12 @@ docker exec -u www-data app-server php occ --no-warnings app:install files_fullt
 docker exec -u www-data app-server php occ --no-warnings app:enable files_external
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://nextcloud_onlyoffice-document-server.1.7cbwky50a2n2kzhigynk2389s/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nextcloud_nginx.1.41d2jknvpgnhdmr3kkdlp8cbt/"
 
 docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch search_platform --value="OCA\\FullTextSearch_Elasticsearch\\Platform\\ElasticSearchPlatform"
 docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch app_navigation --value="1"
-docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch_elasticsearch elastic_host --value="http://elasticsearch_nc:9200/"
+docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch_elasticsearch elastic_host --value="http:// nextcloud_elasticsearch.1.poqsxefnugg2uh88bqhbt56xd:9200/"
 docker exec -u www-data app-server php occ --no-warnings config:app:set fulltextsearch_elasticsearch elastic_index --value="my_index"
 
 docker exec -u www-data app-server php /var/www/html/occ fulltextsearch:index
